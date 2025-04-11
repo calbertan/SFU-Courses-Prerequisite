eb56a9a0d9cbddb2eafa9b7510f148af373d07ea
Insan1tea <tutianhvu@gmail.com>
2025-04-10 20:46:22 -0700

    graph color, legend, size

--- Graph.py
+++ Graph.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import matplotlib.patches as mpatches
 
 # Build course graph
 # df: dataframe containing prerequisites
@@ -99,21 +100,44 @@ def visualize_graph(parent, course_graph, selected_course, on_click):
     # Create the filtered subgraph
     filtered_subgraph, prerequisites = create_filtered_graph(course_graph, selected_course)
 
+    # unlocks for node_colors
+    unlocks = list(filtered_subgraph.successors(selected_course))
+
     # Create a matplotlib figure with a larger size for the graph content
     fig, ax = plt.subplots(figsize=(15, 10))
     pos = custom_layout(filtered_subgraph, selected_course, prerequisites)
 
     # Node colors
-    node_colors = [
-        "lightsteelblue" if node == selected_course else "lightgray"
-        for node in filtered_subgraph.nodes()
-    ]
+    node_colors = []
+    for node in filtered_subgraph.nodes():
+        if node == selected_course:
+            node_colors.append("lightsteelblue")  # Course
+        elif node in prerequisites:
+            node_colors.append("salmon")          # Prereqs 
+        elif node in unlocks:
+            node_colors.append("lightgreen")      # unlocks
+        else:
+            node_colors.append("lightgray")
+
+    # Dynamic node size
+    num_nodes = len(filtered_subgraph.nodes())
+    node_size = max(400, 1500 - (num_nodes * 30))
+
     # Draw the graph
-    nx.draw_networkx_nodes(filtered_subgraph, pos, node_color=node_colors, node_size=1000, ax=ax)
+    nx.draw_networkx_nodes(filtered_subgraph, pos, node_color=node_colors, node_size=node_size, edgecolors='black', ax=ax)
     nx.draw_networkx_labels(filtered_subgraph, pos, font_size=10, ax=ax)
     nx.draw_networkx_edges(filtered_subgraph, pos, edge_color="gray", arrows=True, arrowsize=20, 
                         min_target_margin=20, ax=ax)
     
+    # Legend
+    legend_elements = [
+    mpatches.Patch(color="salmon", label="Prerequisite"),
+    mpatches.Patch(color="lightsteelblue", label="Selected Course"),
+    mpatches.Patch(color="lightgreen", label="Unlocked Course"),
+    mpatches.Patch(color="lightgray", label="Other"),]
+
+    ax.legend(handles=legend_elements, loc="upper right")
+
     # Embed matplotlib figure
     canvas = FigureCanvasTkAgg(fig, master=graph_frame)
     canvas.draw()
